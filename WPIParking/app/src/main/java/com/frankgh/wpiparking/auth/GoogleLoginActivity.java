6ccdf4ae767fa37f0f885266a21a3e235a6a17c5
Francisco Guerrero <frank.guerrero@gmail.com>
2017-11-21 09:17:34 -0500

    Fixed Notifications for Android O from the service.

--- WPIParking/app/src/main/java/com/frankgh/wpiparking/auth/GoogleLoginActivity.java
+++ WPIParking/app/src/main/java/com/frankgh/wpiparking/auth/GoogleLoginActivity.java
@@ -107,6 +107,7 @@ public class GoogleLoginActivity extends BaseActivity {
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
+                            startMainActivity();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
