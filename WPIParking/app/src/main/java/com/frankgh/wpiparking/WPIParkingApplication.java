e94f43774edc8a0e9ad0113b868051cf5e7f9832
Francisco Guerrero <frank.guerrero@gmail.com>
2017-11-21 10:00:38 -0500

    Added code for Google API Keys

---
+++ WPIParking/app/src/main/java/com/frankgh/wpiparking/WPIParkingApplication.java
@@ -0,0 +1,17 @@
+package com.frankgh.wpiparking;
+
+import android.app.Application;
+
+/**
+ * @author Francisco Guerrero <email>me@frankgh.com</email> on 11/21/17.
+ */
+
+public class WPIParkingApplication extends Application {
+
+    private static final String TAG = "WPIParkingApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
