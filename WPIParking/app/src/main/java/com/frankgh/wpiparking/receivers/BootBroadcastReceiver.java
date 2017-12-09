e65bc7b1564af8e6cf348b966a08b196a66ee2ab
Francisco Guerrero <frank.guerrero@gmail.com>
2017-12-09 10:56:42 -0500

    Using new Awareness API for fences for outer and inner geofences

---
+++ WPIParking/app/src/main/java/com/frankgh/wpiparking/receivers/BootBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.frankgh.wpiparking.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author Francisco Guerrero <email>me@frankgh.com</email> on 12/9/17.
+ */
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
