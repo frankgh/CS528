6577616ee359e5dc0c4f60c6fbd9a9efdbaf0d7e
Vaseem Shaikh <vaseem.vs@gmail.com>
2017-12-13 14:41:38 -0500

    - Worked on back button settings page bug (Not Tested)

--- WPIParking/app/src/main/java/com/frankgh/wpiparking/SettingsActivity.java
+++ WPIParking/app/src/main/java/com/frankgh/wpiparking/SettingsActivity.java
@@ -159,7 +159,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                finish();
                 return true;
             }
             return super.onOptionsItemSelected(item);
